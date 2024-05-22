27c0ba3c212289f244eb86619c6934103ae33cee
Brandon Palacio <b.palacio1@utp.edu.co>
2024-05-21 23:39:55 -0500

    fix/taskApp_V0.1.0: Data is fixed by v-model.

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,18 +1,29 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { ITask } from "@/interfaces/Task";
+
 export default defineComponent({
   data() {
     return {
-      task:
-    }
+      task: {} as ITask,
+    };
   },
-})
+  methods: {
+    saveTask() {
+      console.log(this.task);
+    },
+  },
+});
 </script>
 
 <template>
-  <form @submit="">
-    <input type="text" placeholder="Write a title" />
-    <textarea rows="3" placeholder="Write a description"></textarea>
+  <form @submit.prevent="saveTask()">
+    <input type="text" placeholder="Write a title" v-model="task.title" />
+    <textarea
+      rows="3"
+      placeholder="Write a description"
+      v-model="task.description"
+    ></textarea>
     <button>Save</button>
   </form>
 </template>
