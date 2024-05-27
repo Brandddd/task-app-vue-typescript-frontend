65371ae1f68516611fb954a1f209dc0db8decc47
Brandon Palacio <b.palacio1@utp.edu.co>
2024-05-26 20:54:48 -0500

    feat/taskAppFrontend: TaskList component logic completed and api.

--- src/api/TaskApi.ts
+++ src/api/TaskApi.ts
@@ -1,6 +1,22 @@
 import apiBase from "./axiosInstance";
 import { ITask } from "@/interfaces/Task";
 
+// Create
 export const createTask = async (task: ITask) => {
-  await apiBase.post("/tasks", task);
+  try {
+    await apiBase.post("/tasks", task);
+    console.log("Task done!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Read
+export const getTask = async () => {
+  try {
+    const response = await apiBase.get("/tasks");
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Task List Component!</h1>
+  <ul>
+    <li v-for="task in tasks" :key="task._id">
+      {{ task.title }}
+    </li>
+  </ul>
 </template>
+<script lang="ts">
+import { getTask } from "@/api/taskApi";
+import { ITask } from "@/interfaces/Task";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      tasks: [] as ITask[],
+    };
+  },
+  methods: {
+    async loadTaks() {
+      const response = await getTask();
+      this.tasks = response?.data;
+      console.log("data:", response);
+    },
+  },
+  mounted() {
+    this.loadTaks();
+  },
+});
+</script>
